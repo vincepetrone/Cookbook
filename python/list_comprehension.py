5e1f453d8b5fe77a8a54e38658e63b34f771cd08
Vinny Petrone <vincepetrone@yahoo.com>
2021-03-12 22:28:32 -0500

    add to list comprehension file

--- python/list_comprehension.py
+++ python/list_comprehension.py
@@ -1,9 +1,17 @@
 # test list comprehensions
 
 people = [ "Vinny", "Joyce", "Mike", "Bill", "Dana" ]
-greeting = [("Hello, " + person) for person in people]
+#greeting = [("Hello, " + p) for p in people]
 
-print(greeting)
+#print(greeting)
 
-for person in people:
-	print(f"Hello, " + person)
+#for person in people:
+#	print(f"Hello, " + person)
+
+#vips = []
+#for person in people:
+#	if person == "Vinny":
+#		vips.append(person)
+
+vips = [ p for p in people if p == "Vinny" ]
+print(vips)
