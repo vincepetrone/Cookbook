a4295c8ced7ed719faa2a87f48974e3514fb6f11
Vinny Petrone <vincepetrone@yahoo.com>
2021-08-16 11:03:05 -0400

    simplify loop output in show_snaps.sh

--- bash/show_snaps.sh
+++ bash/show_snaps.sh
@@ -3,14 +3,11 @@
 # govc - go command-line utility based on govmomi library for interacting with vSphere API
 # govc man page - https://github.com/vmware/govmomi/blob/v0.24.0/govc/USAGE.md
 
-snap_file=./snapfile.txt
+#snap_file=./snapfile.txt
 declare -a vm_list=( $(cat ./vm_list.txt) )
 
->$snap_file
 for srv in ${vm_list[@]} ; do
-	if govc snapshot.tree -vm $srv | grep -q '.*' ; then      
+	if govc snapshot.tree -vm $srv | grep -q '.' ; then      
 		echo $srv
-		govc snapshot.tree -vm $srv
-		echo
 	fi
 done
